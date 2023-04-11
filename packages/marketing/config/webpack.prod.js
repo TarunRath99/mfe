d9157cb02df8c22bf8007c7b89b38ba31e9dec6b
Tarun Rath <tarun.rath@miqdigital.com>
2023-04-12 01:54:36 +0530

    created deployment script

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        //all different files that are built use this pattern for naming to avoid
+        //caching issues
+        filename: '[name].[contenthash].js'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: ['react', 'react-dom']
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig);
